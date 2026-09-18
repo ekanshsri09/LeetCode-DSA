997c925959b016f443d811f4781cc731f453f474
Ekansh Srivastava <ekanshsrivastava11@gmail.com>
2026-09-18 23:48:44 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (95.52%) - LeetSync

---
+++ 371-sum-of-two-integers/sum-of-two-integers.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+        unsigned int x = a;
+        unsigned int y = b;
+
+        while (y != 0) {
+            unsigned int carry = (x & y) << 1;
+            x = x ^ y;
+            y = carry;
+        }
+
+        return static_cast<int>(x);
+    }
+};
